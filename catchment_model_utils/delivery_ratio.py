2e4808e17d46560a2a49e3267674ee2474453ed0
Joel Rahman <joel@flowmatters.com.au>
2021-06-04 15:54:02 +1000

    Handle missing inputs

--- catchment_model_utils/delivery_ratio.py
+++ catchment_model_utils/delivery_ratio.py
@@ -1,13 +1,32 @@
+from collections import defaultdict
 import pandas as pd
+
+DR_TOLERANCE=1e-2
 
 def _find_starting_points(connectivity):
     conn = connectivity.transpose()
     return list(conn[(conn.sum()==0)].index)
 
 def _compute_dr(site,ds_sites,result,connectivity,local_inputs,outputs,outlet):
+    local_inputs = defaultdict(float,(local_inputs or {}).items())
+    outputs = defaultdict(float,(outputs or {}).items())
     upstream_sites = list(connectivity[connectivity[site]>0].index)
     non_local_inputs = sum([outputs[s] for s in upstream_sites],0.0)
-    dr = outputs[site]/(local_inputs[site]+non_local_inputs)
+    total_output = outputs[site]
+    if total_output == 0.0:
+        dr = 0.0
+    else:
+        total_input = (local_inputs[site]+non_local_inputs)
+        if total_input==0.0:
+            print(f'At {site}, output is {total_output}, but total_input is 0.0')
+        dr = total_output/total_input
+        # if (dr > 1) and (dr < (1+DR_TOLERANCE)):
+        #     dr = 1
+        # if dr > 1:
+        #     print(f'At {site}, output is {total_output}, but total_input is {total_input}, yield a ratio of {dr}')
+        #     print(f'local:{local_inputs[site]},non_local:{non_local_inputs}')
+        #     print(f'upstream sites: {upstream_sites}')
+        #     # assert False
     result.loc[site,site]=dr
     for (ds_site,existing_dr) in ds_sites:
         result.loc[site,ds_site] = existing_dr * dr
